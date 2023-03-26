ac4f388fe5a07cb2c318693d7914ca9aa1d29415
Kuma-0x0 <108416567+Kuma-0x0@users.noreply.github.com>
2023-03-26 18:31:13 +0900

    DurableFunction追加。 (#2)

---
+++ WebAPI.Functions/Functions/Activity/RegisterUserActivity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using WebAPI.Functions.Models;
+
+namespace WebAPI.Functions.Functions.Activity
+{
+    internal class RegisterUserActivity
+    {
+        private readonly ILogger _logger;
+
+        public RegisterUserActivity(ILogger<RegisterUserActivity> logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName(nameof(RunRegisterUserActivity))]
+        public User RunRegisterUserActivity([ActivityTrigger] User user)
+        {
+            _logger.LogInformation($"Input = '{JsonSerializer.Serialize(user)}'");
+            return user;
+        }
+    }
+}
